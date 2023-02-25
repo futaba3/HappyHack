f7a974d6efb17f35fcd1f64d27a2a80bbaff9d6b
Futaba3 <nyokinyoki41aya@gmail.com>
2023-02-25 23:46:40 +0900

    fadein, slider, courses

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 
-$(function($) {
+$(function ($) {
     $(".firstView").bgSwitcher({
         images: ["images/background1.jpg", "images/background2.jpg", "images/background3.jpg", "images/background4.jpg", "images/background5.jpg"],
         interval: 6000,
         loop: true
+    });
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true
+    });
+
+    $('.fadein').css('visibility', 'hidden');
+    $(window).scroll(function () {
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('.fadein').each(function () {
+            var objectPosition = $(this).offset().top;
+            if (topWindow > objectPosition - windowHeight + 200) {
+                $(this).addClass("fadein-anime");
+            }
+        });
     });
 });
