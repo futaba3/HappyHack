f5d72ae69b6762469b7f1c4255d0ed2633da65f3
Futaba3 <nyokinyoki41aya@gmail.com>
2023-02-26 22:07:48 +0900

    fixed firstView's background-image

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(function ($) {
     $(".firstView").bgSwitcher({
         images: ["images/background1.jpg", "images/background2.jpg", "images/background3.jpg", "images/background4.jpg", "images/background5.jpg"],
         interval: 6000,
-        loop: true
+        loop: true,
+        effect: "drop"
     });
 
     
