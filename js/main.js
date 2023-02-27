20b5d1a15e947c75e630f93edef3b21a6080b9f6
Futaba3 <nyokinyoki41aya@gmail.com>
2023-02-27 11:07:15 +0900

    fix animation

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ $(function ($) {
     $(".firstView").bgSwitcher({
         images: ["images/background1.jpg", "images/background2.jpg", "images/background3.jpg", "images/background4.jpg", "images/background5.jpg"],
         interval: 6000,
-        loop: true,
-        effect: "drop"
+        loop: true
     });
 
     
